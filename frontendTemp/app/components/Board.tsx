d77ae5b41af65517bd06ee44c792d64694a3d7b7
AyushAman1208 <ayushaman1208@gmail.com>
2024-12-18 13:57:35 +0000

    not almost finished, a lot more to do

---
+++ frontendTemp/app/components/Board.tsx
@@ -0,0 +1,24 @@
+import { Socket } from 'socket.io-client';
+import InputBox from "./InputBox";
+
+function Board({socket,gameId}:{socket:Socket, gameId:string|null}) {
+    let arr = [1,2,3,4,5,6,7,8]
+  return (
+    <>
+      
+      <div className="flex items-center flex-col justify-center align-middle p-5 border-black">
+        {
+            arr.map?.((row,i) => (
+                <div key={`${row} ${i}`}>{
+                    <div className="flex flex-row p-0" >{arr.map?.((col) => (
+                        <InputBox row = {row} col = {col} key={`${row} ${col}`} socket = {socket} gameId = {gameId} />
+                    ))}</div>
+                }</div>
+            ))
+        }
+      </div>
+    </>
+  );
+}
+
+export default Board;
