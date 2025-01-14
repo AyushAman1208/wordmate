312f7ea68f5d3214d2191ab9bae263c1d35b9cfd
AyushAman1208 <ayushaman1208@gmail.com>
2025-01-14 23:08:27 +0000

    ss

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,5 +1,5 @@
 module.exports = {
   env: {
-    "SOCKET_URL": "https://wordmate-server-5swkptsl2-ayush-amans-projects.vercel.app/" 
+    "SOCKET_URL": "https://wordmate-server-5swkptsl2-ayush-amans-projects.vercel.app" 
   },
 }
