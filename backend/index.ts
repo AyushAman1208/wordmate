f393f55d562cfcb09eaafdd53f5ca9a377720233
AyushAman1208 <ayushaman1208@gmail.com>
2025-01-14 20:26:02 +0000

    change cors in backend

--- backend/index.ts
+++ backend/index.ts
@@ -8,7 +8,7 @@ import 'dotenv/config'
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: process.env.FRONTEND_URL, methods: ["GET", "POST"] },
+  cors: { origin: "*", methods: ["GET", "POST"] },
 });
 let waitingPlayer: any = null; // Stores the socket and name of the player waiting for an opponent
 const games: any = {}; // Tracks active games by gameId
